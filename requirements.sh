b680f0b1085f6f8132c4385a2f77215ef1b4d03d
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-10-13 06:45:47 -0400

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/JoeySoprano420/ML-Plus.git /path/to/virtual/ML-Plus
+git clone https://github.com/JoeySoprano420/HTML-C.git /path/to/virtual/HTML-C
+git clone https://github.com/JoeySoprano420/MDRI-FILES.git /path/to/virtual/MDRI-FILES
+git clone https://github.com/JoeySoprano420/EAL-Environment.git /path/to/virtual/EAL
