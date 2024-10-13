54250e070932e324eab25c8aa8b9829ea643f33c
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-10-13 09:56:39 -0400

    Create Setting Up Multi-Threading.js

---
+++ ALTERNATE ENVIRONMENT/Setting Up Multi-Threading.js
@@ -0,0 +1,12 @@
+const { Worker } = require('worker_threads');
+
+function runWorker(workerData) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker('./worker.js', { workerData });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        worker.on('exit', (code) => {
+            if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+    });
+}
